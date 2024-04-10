3031ac026ff343fb023ff2e127de428d4b4aa610
Nishant <nishkohli96@gmail.com>
2024-04-11 02:40:59 +0530

    modify rules

--- __tests__/bad-js.js
+++ __tests__/bad-js.js
@@ -8,6 +8,9 @@ const obj = { name: 'john', age: 36}
 
 function someFN(var1,var2,var3) {return var1-var2+var3}
 
+let arr = [2,42,42,33,23,67,234,2323]
+
+arr.forEach(ele => ele+=1)
 
 // const element = () => {
 // 	return (
