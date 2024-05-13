d409a5eb53e9f2457fd190cf0026832691a3844d
Nishant <nishkohli96@gmail.com>
2024-05-14 01:23:50 +0530

    integrate stylistic

--- packages/eslint-config-flat/lib/js/index.mjs
+++ packages/eslint-config-flat/lib/js/index.mjs
@@ -1,11 +1,84 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
+    plugins: {
+      '@stylistic': stylistic,
+    },
     rules: {
+      '@stylistic/array-bracket-newline': [
+        'warn',
+        { multiline: true, minItems: 4 },
+      ],
+      '@stylistic/array-bracket-spacing': 'warn',
+      '@stylistic/array-element-newline': [
+        'warn',
+        { multiline: true, minItems: 4 },
+      ],
+      '@stylistic/arrow-parens': ['warn', 'as-needed'],
+      '@stylistic/arrow-spacing': 'warn',
+      '@stylistic/block-spacing': 'warn',
+      '@stylistic/brace-style': 'warn',
+      '@stylistic/comma-dangle': [
+        'warn',
+        {
+          arrays: 'never',
+          objects: 'never',
+          imports: 'never',
+          exports: 'never',
+          functions: 'never',
+        },
+      ],
+      '@stylistic/comma-spacing': 'warn',
+      '@stylistic/eol-last': 'warn',
+      '@stylistic/function-call-argument-newline': ['warn', 'consistent'],
+      '@stylistic/function-paren-newline': ['warn', 'consistent'],
+      '@stylistic/indent': ['warn', 2],
+      '@stylistic/indent-binary-ops': ['warn', 2],
+      '@stylistic/key-spacing': 'warn',
+      '@stylistic/linebreak-style': 'warn',
+      '@stylistic/newline-per-chained-call': 'warn',
+      '@stylistic/no-extra-semi': 'warn',
+      '@stylistic/no-floating-decimal': 'warn',
+      '@stylistic/no-mixed-operators': 'error',
+      '@stylistic/no-mixed-spaces-and-tabs': 'warn',
+      '@stylistic/no-multi-spaces': 'warn',
+      '@stylistic/no-multiple-empty-lines': 'warn',
+      '@stylistic/no-trailing-spaces': 'warn',
+      '@stylistic/object-curly-newline': ['warn', { multiline: true }],
+      '@stylistic/object-curly-spacing': ['warn', 'always'],
+      '@stylistic/object-property-newline': 'warn',
+      '@stylistic/operator-linebreak': ['warn', 'before'],
+      '@stylistic/quotes': ['warn', 'single'],
+      '@stylistic/rest-spread-spacing': 'warn',
+      '@stylistic/semi-spacing': 'warn',
+      '@stylistic/space-before-blocks': [
+        'warn',
+        {
+          functions: 'always',
+          classes: 'always',
+          keywords: 'always',
+        },
+      ],
+      '@stylistic/space-infix-ops': 'warn',
+      '@stylistic/space-unary-ops': [
+        'warn',
+        {
+          words: true,
+          nonwords: false,
+        },
+      ],
+      '@stylistic/spaced-comment': 'warn',
+      '@stylistic/switch-colon-spacing': 'warn',
+      '@stylistic/template-curly-spacing': 'warn',
+      '@stylistic/type-annotation-spacing': 'warn',
+      '@stylistic/type-generic-spacing': 'warn',
+      '@stylistic/type-named-tuple-spacing': 'warn',
+      '@stylistic/wrap-regex': 'warn',
       '@typescript-eslint/ban-ts-comment': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-unused-vars': 'warn',
