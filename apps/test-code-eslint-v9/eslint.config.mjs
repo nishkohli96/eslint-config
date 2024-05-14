323390db4cd9fa9809aedd9b6d61149044a2dd39
Nishant <nishkohli96@gmail.com>
2024-05-14 23:42:10 +0530

    rename to eslint-flat-config

--- apps/test-code-eslint-v9/eslint.config.mjs
+++ apps/test-code-eslint-v9/eslint.config.mjs
@@ -3,15 +3,13 @@
  * 
  * https://eslint.org/docs/latest/use/configure/migration-guide#predefined-and-shareable-configs
  */
-// import js from '@eslint/js';
-import jsEslintConfig from '@nish1896/eslint-config-flat/js';
-import reactEslintConfig from '@nish1896/eslint-config-flat/react';
+import jsEslintConfig from '@nish1896/eslint-flat-config/js';
+import reactEslintConfig from '@nish1896/eslint-flat-config/react';
 
 export default [
-  // js.configs.recommended,
   ...jsEslintConfig,
   ...reactEslintConfig, 
-  // {
-  //   rules: {}
-  // }
+  {
+    rules: {}
+  }
 ];
