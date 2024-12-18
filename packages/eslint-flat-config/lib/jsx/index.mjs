ca9f4a2e011151342b74fd9882633ee1e0a35f1c
Nishant <nishkohli96@gmail.com>
2024-12-18 16:30:58 +0530

    rm project from parserOptions

--- packages/eslint-flat-config/lib/jsx/index.mjs
+++ packages/eslint-flat-config/lib/jsx/index.mjs
@@ -15,7 +15,6 @@ export default [
         ecmaFeatures: {
           jsx: true,
         },
-        project: './tsconfig.json'
       },
     },
     settings: {
--- packages/eslint-flat-config/lib/next/index.mjs
+++ packages/eslint-flat-config/lib/next/index.mjs
@@ -30,7 +30,6 @@ export default [
         ecmaFeatures: {
           jsx: true,
         },
-        project: './tsconfig.json'
       },
     },
     settings: {
