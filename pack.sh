b66646e01e840c432a8980c8132de689457dc176
Nishant <nishkohli96@gmail.com>
2025-01-25 02:42:03 +0530

    increase version numbers and script to pack

---
+++ pack.sh
@@ -0,0 +1,12 @@
+# This script will generate the tgz file for the each eslint
+# package in the packages directory.
+
+echo '🏁 Initiating Packing...'
+
+cd ./packages/eslint-config
+npm pack
+echo "Packed 📦: @nish1896/eslint-config successfully! 🥳🥳🥳"
+
+cd ../eslint-flat-config
+npm pack
+echo "Packed 📦: @nish1896/eslint-flat-config successfully! 🥳🥳🥳"
