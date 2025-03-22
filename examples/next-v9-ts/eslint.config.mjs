def5f8bc56dbd952a67b9f70ee4bb83401e4b5b6
Nishant <nishkohli96@gmail.com>
2025-03-22 05:42:01 +0530

    wip

--- examples/next-v9-ts/eslint.config.mjs
+++ examples/next-v9-ts/eslint.config.mjs
@@ -1,6 +1,9 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import jsConfig from "@nish1896/eslint-flat-config/js";
+import tsConfig from "@nish1896/eslint-flat-config/ts";
+import nextConfig from "@nish1896/eslint-flat-config/next";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +14,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...jsConfig,
+  // ...tsConfig,
+  ...nextConfig,
+  {
+    rules: {
+      'jsx-a11y/anchor-ambiguous-text': 'error',
+      'jsx-a11y/control-has-associated-label': 'error',
+      '@next/next/no-duplicate-head': 'off' // Disable the problematic rule
+    }
+  }
 ];
 
 export default eslintConfig;
