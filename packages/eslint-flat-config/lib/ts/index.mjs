e1b56056a4599a7feb1af5246387582731aae555
Nishant <nishkohli96@gmail.com>
2025-03-25 01:53:52 +0530

    add more warning and off to ts

--- packages/eslint-flat-config/lib/ts/index.mjs
+++ packages/eslint-flat-config/lib/ts/index.mjs
@@ -50,11 +50,16 @@ export default [
       '@typescript-eslint/consistent-generic-constructors': 'warn',
       '@typescript-eslint/consistent-type-definitions': 'off',
       '@typescript-eslint/consistent-type-exports': 'warn',
+      '@typescript-eslint/no-base-to-string': 'warn',
       '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-floating-promises': 'off',
       '@typescript-eslint/no-inferrable-types': 'off',
       '@typescript-eslint/no-misused-promises': 'off',
       '@typescript-eslint/no-this-alias': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn'
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/restrict-template-expressions': 'warn',
     },
   },
   {
