b6b68109eb45ae926d24eba74c26d76ef76c5e15
Nishant Kohli <nishkohli96@gmail.com>
2025-03-27 15:36:48 +0530

    wip

--- examples/eslint-8/js-v8/__tests__/bad-jsx.js
+++ examples/eslint-8/js-v8/__tests__/bad-jsx.js
@@ -3,10 +3,10 @@ import { Button, Grid, Radio, FormControl } from '@mui/material';
 localStorage.setItem('test', '123');
 expect(Promise.resolve('lemon')).resolves.toBe('lemon');
 
-// const element = () => {
-// 	return (
-// 		<div>
-// 			<img src='' />
-// 		</div>
-// 	)
-// }
+const element = () => {
+	return (
+		<div>
+			<img src='' />
+		</div>
+	)
+}
