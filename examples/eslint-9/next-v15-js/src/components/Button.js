a2bf6912faa59e5d4b424143362aedb560e5ee55
Nishant <nishkohli96@gmail.com>
2025-04-22 20:30:28 +0530

    add button.js in next-v15

---
+++ examples/eslint-9/next-v15-js/src/components/Button.js
@@ -0,0 +1,10 @@
+export default function Button({ children, ...props }) {
+	return (
+		<button
+			{...props}
+			className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+		>
+			{children}
+		</button>
+	);
+}
