70f6f092c6239b4031148c22735239ce70750f73
Nishant <nishkohli96@gmail.com>
2026-05-31 21:30:08 +0530

    Update dependencies for nextjs pkg

--- examples/eslint-v10/next-v16/eslint.config.mjs
+++ examples/eslint-v10/next-v16/eslint.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import jsConfig from '@nish1896/eslint-flat-config/js';
+import nextConfig from '@nish1896/eslint-flat-config/next';
+import nextTsConfig from '@nish1896/eslint-flat-config/next-ts';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  ...jsConfig,
+  ...nextConfig,
+  ...nextTsConfig,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
